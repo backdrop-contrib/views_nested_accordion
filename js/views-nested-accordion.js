ce210de07edc57ec3583e44e064e36c43bbafe13
Anil Kumar <anil280988@gmail.com>
2014-08-27 11:51:27 +0530

    final change

--- js/views-nested-accordion.js
+++ js/views-nested-accordion.js
@@ -2,23 +2,28 @@
  * @file
  * JS for Nested Accordion.
  */
+(function ($) {
+
 Drupal.behaviors.views_nested_accordion = {
   attach: function(context) {
     if(Drupal.settings.views_nested_accordion){
-      (function ($) {
        
         $.each(Drupal.settings.views_nested_accordion, function(id) {
           /* Our Nested Accordion Settings */
           var viewname = this.viewname;
+          /* Add Class to the Group Header On Page Load */
           $( '.view-id-' + viewname + ' .view-grouping:first-child .view-grouping-header' ).addClass('nested-accordion');
+          /* Generate Accordion Effect on Outer Header Click */
           $( '.view-id-' + viewname + ' .view-grouping .view-grouping-header' ).click(function() {
 
                   var attrib = $(this).attr("class");
                   var remove = attrib.split(" ");
-                  if (remove[1] == 'nested-accordion') {//alert(remove[1]);
+                  if (remove[1] == 'nested-accordion') {
+                    /* If Accordion is Open, then Clicking on it will close the Accordion. */
                     $(this).removeClass("nested-accordion");
                     $(this).siblings('.view-grouping-content').slideUp();
                   } else {
+                    /* Clicking on Header will Open the Accordion */
                     $(this).addClass('nested-accordion');
                     $(this).siblings('.view-grouping-content').slideDown();
                     $(this).parents('.view-grouping').siblings('.view-grouping').children('.view-grouping-header').removeClass('nested-accordion');
@@ -27,7 +32,8 @@ Drupal.behaviors.views_nested_accordion = {
           
           });    
         });
-      })(jQuery);
     }
   }
 };
+
+})(jQuery);
