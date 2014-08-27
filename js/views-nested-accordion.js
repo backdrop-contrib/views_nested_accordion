4086a9cd084cff88e484ba12d842fd42ee40f846
Anil Kumar <anil280988@gmail.com>
2014-08-27 12:18:34 +0530

    atomatic review

--- js/views-nested-accordion.js
+++ js/views-nested-accordion.js
@@ -2,19 +2,20 @@
  * @file
  * JS for Nested Accordion.
  */
+
 (function ($) {
 
 Drupal.behaviors.views_nested_accordion = {
   attach: function(context) {
     if(Drupal.settings.views_nested_accordion){
-       
+      
         $.each(Drupal.settings.views_nested_accordion, function(id) {
           /* Our Nested Accordion Settings */
           var viewname = this.viewname;
           /* Add Class to the Group Header On Page Load */
-          $( '.view-id-' + viewname + ' .view-grouping:first-child .view-grouping-header' ).addClass('nested-accordion');
+          $('.view-id-' + viewname + ' .view-grouping:first-child .view-grouping-header').addClass('nested-accordion');
           /* Generate Accordion Effect on Outer Header Click */
-          $( '.view-id-' + viewname + ' .view-grouping .view-grouping-header' ).click(function() {
+          $('.view-id-' + viewname + ' .view-grouping .view-grouping-header').click(function() {
 
                   var attrib = $(this).attr("class");
                   var remove = attrib.split(" ");
@@ -29,8 +30,8 @@ Drupal.behaviors.views_nested_accordion = {
                     $(this).parents('.view-grouping').siblings('.view-grouping').children('.view-grouping-header').removeClass('nested-accordion');
                     $(this).parents('.view-grouping').siblings('.view-grouping').children('.view-grouping-content').slideUp();
                   }
-          
-          });    
+                  
+          });
         });
     }
   }
