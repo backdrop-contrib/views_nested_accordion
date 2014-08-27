66fc253f0a526a83ab961ce1b0444e4ad938ea73
Anil Kumar <anil280988@gmail.com>
2014-08-27 12:21:00 +0530

    atomatic review

--- js/views-nested-accordion.js
+++ js/views-nested-accordion.js
@@ -8,7 +8,7 @@
 Drupal.behaviors.views_nested_accordion = {
   attach: function(context) {
     if(Drupal.settings.views_nested_accordion){
-    
+   
         $.each(Drupal.settings.views_nested_accordion, function(id) {
           /* Our Nested Accordion Settings */
           var viewname = this.viewname;
